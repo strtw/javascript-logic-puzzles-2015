bc577428ef514f463bb545ba2a01bf147d2fc729
strtw <strtw@users.noreply.github.com>
2015-07-30 14:19:55 -0700

    Create exercise5.js
    Write a JavaScript program that accepts a number as input and insert dashes (-) between each two even numbers. For example if you accept 025468 the output should be 0-254-6-8.

---
+++ week-4-logic/exercise5.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function dashNumbers(n){
+    var i;
+    var numString = n.toString();
+    var numStringArray = numString.split("");
+    for (i=0; i<numStringArray.length; i++){
+        if(numStringArray[i] % 2 === 0 && numStringArray[i-1] % 2 === 0){
+	    numStringArray[i] = "-" + numStringArray[i];
+		}
+	}
+	var returnString = numStringArray.join("");
+	return returnString;
+}
