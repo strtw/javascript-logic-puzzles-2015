a048f09340d6842c7bdf31627de23db012ac4f8d
strtw <strtw@users.noreply.github.com>
2015-08-14 12:33:00 -0700

    Create coderbytes8.js
    Using the JavaScript language, have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than num1, otherwise return the string false. If the parameter values are equal to each other then return the string -1.

---
+++ week-4-logic/coderbytes8.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function CheckNums(num1, num2) {
+    if (num2 > num1) {
+        return "true";
+    } else if (num2 === num1) {
+        return "-1";
+    } else {
+        return "false";
+    }
+}
